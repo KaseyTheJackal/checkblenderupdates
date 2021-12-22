72849203705fea8c4f838326120b83a90725ef85
Kasey <58578837+KaseyTheJackal@users.noreply.github.com>
2021-12-22 15:19:49 -0600

    Update checkup.py

--- checkup.py
+++ checkup.py
@@ -1,7 +1,10 @@
 import unittest
+import os
+from datetime import datetime
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-driver = webdriver.driver
+driver = webdriver.Safari
 
 driver.get("https://builder.blender.org/download/daily")
 driver.title("Architecture")
