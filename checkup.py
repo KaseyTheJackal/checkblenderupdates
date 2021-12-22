26f3ad7a178c3512ccbab9c4e339edaf273573a6
Kasey <58578837+KaseyTheJackal@users.noreply.github.com>
2021-12-22 15:42:44 -0600

    Update checkup.py

--- checkup.py
+++ checkup.py
@@ -4,8 +4,13 @@ from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Safari
+driver = webdriver.Safari()
 
 driver.get("https://builder.blender.org/download/daily")
-driver.title("Architecture")
+items = driver.find_elements(By.ID, "builds-list platform-darwin active")
 
+for item in items:
+    text = item.text
+    print(text)
+
+driver.close()
